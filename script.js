e05740a765e1ec02074c139b35561048d91b31c0
netflys <76844629+netflys@users.noreply.github.com>
2021-01-08 15:27:49 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// https://netflys.github.io/script.js
 
+$(document).ready(function(w) {
+  $("#get-started").click(function (_this) {
+    _this = this;
+    window.location.href = "../get-started/index.html";
+  });
+});
